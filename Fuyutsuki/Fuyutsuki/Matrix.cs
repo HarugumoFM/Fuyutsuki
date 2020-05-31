60ace3601d72c88eb24c44e49575d972e072da80
HarugumoFumi <57428942+HarugumoFumi@users.noreply.github.com>
2020-05-31 13:17:00 +0900

    Add files via upload

---
+++ Fuyutsuki/Fuyutsuki/Matrix.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuyutsuki
+{
+    [Serializable]
+    public class Matrix
+    {
+        public int Row;
+        public int Column;
+        public double[,] Data;
+        public Matrix()
+        { }
+        public Matrix(double[,] data)
+        {
+            Data = data;
+            Row = data.GetLength(0);
+            Column = data.GetLength(1);
+
+        }
+        public Matrix(double data)
+        {
+            Data = new double[1, 1] { { data } };
+
+            Row = 1;
+            Column = 1;
+
+        }
+        public Matrix Mul(Matrix a, Matrix b)
+        {
+            int n = a.Column;
+            var result = CreateMatrix(a.Row, b.Column);
+            for (int i = 0; i < a.Row; i++)
+            {
+                for (int j = 0; j < b.Column; j++)
+                {
+                    double sum = 0;
+                    for (int k = 0; k < n; k++)
+                    {
+                        sum += a.Data[i, k] * b.Data[k, j];
+                    }
+                    result.Data[i, j] = sum;
+
+                }
+            }
+            return result;
+        }
+        public Matrix Mul(Matrix a, double b)
+        {
+            int n = a.Column;
+            var result = CreateMatrix(a.Row, a.Column);
+            for (int i = 0; i < a.Row; i++)
+            {
+                for (int j = 0; j < a.Column; j++)
+                {
+                    result.Data[i, j] *= b;
+
+                }
+            }
+            return result;
+        }
+        public Matrix CreateMatrix(int row, int column)
+        {
+            Matrix result = new Matrix(new double[row, column]);
+            return result;
+
+
+        }
+
+        public Matrix Exp(Matrix a)
+        {
+            var result = CreateMatrix(a.Row, a.Column);
+            for (int i = 0; i < a.Row; i++)
+            {
+                for(int j=0;j<a.Column;j++)
+                {
+                    result.Data[i, j] = Math.Exp(a.Data[i, j]);
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ Fuyutsuki/Fuyutsuki/Tool.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.Serialization;
+
+namespace Fuyutsuki
+{
+    [Serializable]
+    public class Tool
+    {
+        public class Function:Matrix
+        {
+            public Matrix Data;
+            public Matrix Gradient;
+            
+            Matrix Forward(Matrix num)
+            {
+                Data = num;
+                return num;
+            }
+            Matrix Backward(Matrix num)
+            {
+                Gradient = num;
+                return num;
+            }
+        }
+        public class Square : Function
+        {
+            public Matrix Forward(Matrix num)
+            {
+                this.Data = num;
+                return Mul(Data, Data);
+
+            }
+            public Matrix Backward(Matrix gx)
+            {
+                return Mul(Mul(gx, 2), gx);
+            }
+        }
+        public class Exp : Function
+        {
+            public Matrix Forward(Matrix num)
+            {
+                this.Data = num;
+                return Exp(num);
+            }
+            public Matrix BackWard(Matrix gy)
+            {
+                return Mul(Data, Mul(Data, gy));
+            }
+        }
+    }
+   
+}
